b7fd02dd6bbef54200ee6fb5afb2015afe55fe96
endruxa <nesterov.andrey.a@gmail.com>
2018-09-04 23:27:04 +0300

    markdown_email

---
+++ app/Mail/newMail.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class newMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * newMail constructor.
+     */
+
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('nesterov.andrey.a@yandex.ru')->markdown('email')->to('nesterov.andrey.a@yandex.ru');
+    }
+}
